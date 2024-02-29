c2589b893acc0703b6e4e0ddbe7110f2f1c9f33c
Ethan <eluk0902@gmail.com>
2024-02-29 17:04:58 -0500

    moving fetch functionality to table comnponent

--- src/App.tsx
+++ src/App.tsx
@@ -2,39 +2,11 @@ import '@mantine/core/styles.css';
 
 import { MantineProvider } from '@mantine/core';
 import SubjectTable from './SubjectTable';
-import Subject from './models/subject';
-import { useEffect, useState } from 'react';
-
 
 export default function App() {
-  const [subjects, setSubjects] = useState<Subject[]>([])
-  const [isLoadingData, setIsLoadingData] = useState<boolean>(true)
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch('https://055d8281-4c59-4576-9474-9b4840b30078.mock.pstmn.io/subjects');
-        const result = (await response.json()).data as Subject[]
-
-        setSubjects(result);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
-
-    fetchData();
-  }, []);
-
-  useEffect(() => {
-    setIsLoadingData(false)
-    console.log(subjects)
-  }, [subjects])
-
   return (
     <MantineProvider>
-      {
-        !isLoadingData && <SubjectTable subjects={subjects}/>
-      }
+      <SubjectTable />
     </MantineProvider>
   )
 }
--- src/SubjectTable.tsx
+++ src/SubjectTable.tsx
@@ -1,29 +1,67 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 import Subject from './models/subject';
 import { Table } from '@mantine/core';
 
+function SubjectTable() {
 
-interface ChildProps {
-  subjects: Subject[];
-}
+  const [subjects, setSubjects] = useState<Subject[]>([])
+  const [isLoadingData, setIsLoadingData] = useState<boolean>(false)
 
-  const SubjectTable: React.FC<ChildProps> = ({ subjects }) => {
-    return (
-      <>
-        <Table>
-          <Table.Tbody>
-            {subjects.map((subject) => (
-              <Table.Tr key={subject.id}>
-                <Table.Td>{subject.id}</Table.Td>
-                <Table.Td>{subject.name}</Table.Td>
-                <Table.Td>{subject.age}</Table.Td>
-                <Table.Td>{subject.gender}</Table.Td>
-              </Table.Tr>
-            ))}
-          </Table.Tbody>
-        </Table>
-      </>
-    );
-  };
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setIsLoadingData(true);
+        const response = await fetch('https://055d8281-4c59-4576-9474-9b4840b30078.mock.pstmn.io/subjects');
+        const result = (await response.json()).data as Subject[]
 
-  export default SubjectTable;
+        setSubjects(result);
+        setIsLoadingData(false);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    console.log(subjects)
+  }, [subjects])
+
+  const ths = (
+    <Table.Tr>
+      <Table.Th>Subject Id</Table.Th>
+      <Table.Th>Name</Table.Th>
+      <Table.Th>Age</Table.Th>
+      <Table.Th>Gender</Table.Th>
+      <Table.Th>Diagnosis Data</Table.Th>
+      <Table.Th>Status</Table.Th>
+    </Table.Tr>
+  );
+
+  const rows = subjects.map((subject) => (
+    <Table.Tr key={subject.id}>
+      <Table.Td>{subject.id}</Table.Td>
+      <Table.Td>{subject.name}</Table.Td>
+      <Table.Td>{subject.age}</Table.Td>
+      <Table.Td>{subject.gender}</Table.Td>
+      <Table.Td>{subject.diagnosisDate}</Table.Td>
+      <Table.Td>{subject.status}</Table.Td>
+    </Table.Tr>
+  ))
+
+  return (
+    <>
+      {
+        isLoadingData ? 
+          <p>Loading...</p> : 
+          <Table striped highlightOnHover withTableBorder withColumnBorders>
+            <Table.Thead>{ths}</Table.Thead>
+            <Table.Tbody>{rows}</Table.Tbody>
+          </Table>
+      }
+    </>
+  );
+};
+
+export default SubjectTable;
